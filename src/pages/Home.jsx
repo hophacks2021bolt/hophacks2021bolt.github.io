14fff22fb81bdb09c3d17a1d829739e380cc3073
g-kienzle <kienzle2003@gmail.com>
2021-09-18 00:38:31 -0400

    Added splash screen

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css';
+
+
+function Home() {
+  return (
+    
+    <div className="home">
+      <div class="Splash-page">
+        <h1>KiloWhat</h1>
+        a worldwide energy comparison tool 
+      </div>
+    </div>
+  );
+}
+
+export default Home;
