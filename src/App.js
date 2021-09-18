d99fa56c06a2e1c34c0b84d30220643c11dfbf1d
Kandasamy Chokkalingam <samchoks@hotmail.com>
2021-09-18 09:33:42 -0400

    Remove unused logo from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {Home} from './pages';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
